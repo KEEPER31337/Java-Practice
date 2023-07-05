2d92407e6f71a8175a7f182a01e8433f4568d87b
02ggang9 <02ggang9@gmail.com>
2023-07-05 19:49:51 +0900

    p1

--- src/main/java/Problem/shkisme/Chapter1/HelloWorld.java
+++ src/main/java/Problem/shkisme/Chapter1/HelloWorld.java
@@ -3,4 +3,8 @@ package Problem.shkisme.Chapter1;
 public class HelloWorld {
 
   // TODO: Hello World 출력하기
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
 }

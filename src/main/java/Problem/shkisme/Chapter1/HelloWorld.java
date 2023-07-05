48b9fe5f92d6983ef96cf7afa9e42c220943a3d0
redzzzi <redzzzi@pusan.ac.kr>
2023-07-05 19:49:00 +0900

    printhelloinjava

--- src/main/java/Problem/shkisme/Chapter1/HelloWorld.java
+++ src/main/java/Problem/shkisme/Chapter1/HelloWorld.java
@@ -2,5 +2,8 @@ package Problem.shkisme.Chapter1;
 
 public class HelloWorld {
 
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
   // TODO: Hello World 출력하기
 }

da0b25032b22b6e7853f5db49649db00af11c291
stopmin <geemin0@naver.com>
2023-07-07 22:35:16 +0900

    solved:Chapter3-2

--- src/main/java/Problem/stopmin/Chapter3/Q02.java
+++ src/main/java/Problem/stopmin/Chapter3/Q02.java
@@ -5,9 +5,21 @@ public class Q02 {
   /**
    * 홀수 짝수 판별
    */
+  public static boolean isOdd(int a) {
+    if (a % 2 == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   public static void main(String[] args) {
     int num = 13;
 
-    // TODO: num이 홀수라면 odd를, 짝수라면 even을 출력하기
+    if (isOdd(num)) {
+      System.out.println("Odd");
+    } else {
+      System.out.println("even");
+    }
   }
 }

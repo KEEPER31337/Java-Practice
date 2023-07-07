fc0eaa47e09e7c3c622f14e2149190a0e093f00e
stopmin <geemin0@naver.com>
2023-07-08 00:38:17 +0900

    solved:Chapter4-5

--- src/main/java/Problem/stopmin/Chapter4/Q05.java
+++ src/main/java/Problem/stopmin/Chapter4/Q05.java
@@ -9,5 +9,11 @@ public class Q05 {
     int[] marks = {70, 60, 55, 75, 95, 90, 80, 80, 85, 100};
 
     // TODO: for each 문을 사용하여 A 학급의 평균 점수를 구해 보자.
+    int average = 0;
+
+    for (int entry : marks) {
+      average += entry;
+    }
+    System.out.println(average / marks.length);
   }
 }

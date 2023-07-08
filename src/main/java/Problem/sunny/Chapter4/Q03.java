0ee8b7c78aa16a8c27a0be04395151fd5783ff94
ggomabbang <badawa7@naver.com>
2023-07-08 21:01:58 +0900

    problem34-sunny

--- src/main/java/Problem/sunny/Chapter4/Q03.java
+++ src/main/java/Problem/sunny/Chapter4/Q03.java
@@ -7,7 +7,12 @@ public class Q03 {
    */
   public static void main(String[] args) {
     // TODO: while문 또는 for 문을 사용하여 다음과 같이 별(*)을 표시하는 프로그램을 작성해 보자.
+    String stars = "";
 
+    for (int i=0; i<5; i++){
+      stars += "*";
+      System.out.println(stars);
+    }
     //*
     //**
     //***

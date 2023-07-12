a67f8cf463ead39d2de9fb55163e418fe651a7a6
kimminjun <kimminjun514@gmail.com>
2023-07-12 18:10:58 +0900

    Chapter 05 practice

---
+++ src/main/java/Practice/Ch05/_02/Ch05_02.java
@@ -0,0 +1,24 @@
+package Practice.Ch05._02;
+
+class Animal {
+
+  String name;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
+
+public class Ch05_02 {
+
+  public static void main(String[] args) {
+    Animal cat = new Animal();
+    cat.setName("boby");
+
+    Animal dog = new Animal();
+    dog.setName("happy");
+
+    System.out.println(cat.name);
+    System.out.println(dog.name);
+  }
+}

78a543db89c07afe2e0553238674f51cc7f747f1
lcqff <yeonnu82g@gmail.com>
2023-07-16 19:09:03 +0900

    Q06

--- src/main/java/Problem/lcqff/Chapter5/Q06.java
+++ src/main/java/Problem/lcqff/Chapter5/Q06.java
@@ -10,5 +10,8 @@ public class Q06 {
     Calculator cal = new Calculator();
     cal.add(3);  // 여기서 NullPointerException 이 발생한다.
     System.out.println(cal.getValue());
+
+    // Integer 클래스를 사용해 선언된 변수는 null로 초기화 된다. 따라서 초기화 하지 않고 사용할 경우 NullPointException이 발생한다.
+    // int로 선언된 변수는 기본적으로 0으로 초기화된다. 따라서 따로 초기화 하지 않아도 NullPointException이 발생하지 않는다.
   }
 }

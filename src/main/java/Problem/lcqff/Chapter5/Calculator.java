da8de4974199ff5ce55d1b0ffa799e9643862af3
lcqff <yeonnu82g@gmail.com>
2023-07-16 18:46:23 +0900

    Q04

--- src/main/java/Problem/lcqff/Chapter5/Calculator.java
+++ src/main/java/Problem/lcqff/Chapter5/Calculator.java
@@ -1,11 +1,25 @@
 package Problem.lcqff.Chapter5;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
   int value;
 
   Calculator() {
     this.value = 0;
+  }
+
+  int avg(int[] data) {
+    int sum = 0;
+    for (int num:data) sum += num;
+    return sum/ data.length;
+  }
+
+  int avg(ArrayList<Integer> data) {
+    int sum = 0;
+    for (int num:data) sum += num;
+    return sum/ data.size();
   }
 
   boolean isOdd(int num) {

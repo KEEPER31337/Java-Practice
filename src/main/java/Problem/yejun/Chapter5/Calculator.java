8c6da84e313add9ac413d3bd941c6ec77a075f38
신예준 <wns1826@gmail.com>
2023-07-19 18:05:56 +0900

    problem5-yejun

--- src/main/java/Problem/yejun/Chapter5/Calculator.java
+++ src/main/java/Problem/yejun/Chapter5/Calculator.java
@@ -1,5 +1,7 @@
 package Problem.yejun.Chapter5;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
   int value;
@@ -16,4 +18,23 @@ public class Calculator {
     return this.value;
   }
 
+  boolean isOdd(int val) {
+    return (val % 2 == 1);
+  }
+
+  int avg(int[] arr) {
+    int sum = 0;
+    for (int n : arr) {
+      sum += n;
+    }
+    return sum / arr.length;
+  }
+
+  int avg(ArrayList<Integer> arr) {
+    int sum = 0;
+    for (int n : arr) {
+      sum += n;
+    }
+    return sum / arr.size();
+  }
 }
---
+++ src/main/java/Problem/yejun/Chapter5/Q7/Mineral.java
@@ -0,0 +1,6 @@
+package Problem.yejun.Chapter5.Q7;
+
+public interface Mineral {
+
+  int getValue();
+}
